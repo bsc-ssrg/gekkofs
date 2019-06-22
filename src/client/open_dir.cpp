4fe30c01a66b9a4f0781f793d302edf663f2f958
Ramon Nou <ramon.nou@bsc.es>
2019-06-22 20:09:32 +0100

    Solved dirent bug, unsigned int -> unsigned long long for pos

--- src/client/open_dir.cpp
+++ src/client/open_dir.cpp
@@ -39,7 +39,7 @@ void OpenDir::add(const std::string& name, const FileType& type) {
     entries.push_back(DirEntry(name, type));
 }
 
-const DirEntry& OpenDir::getdent(unsigned int pos) {
+const DirEntry& OpenDir::getdent(unsigned long long pos) {
     return entries.at(pos);
 }
 
