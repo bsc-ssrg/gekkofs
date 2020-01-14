ec35a6af39cbf6b888339baa1a0817ee936ae035
Ramon Nou <ramon.nou@bsc.es>
2020-01-14 10:59:08 +0000

    Update dl_dep.sh

--- scripts/dl_dep.sh
+++ scripts/dl_dep.sh
@@ -271,7 +271,7 @@ if [ "${NA_LAYER}" == "ofi" ] || [ "${NA_LAYER}" == "all" ]; then
    fi
     # No need to get libfabric for mogon2 as it is already installed
     if [[ ("${CLUSTER}" != "mogon2") ]]; then
-        wgetdeps "libfabric" "https://github.com/ofiwg/libfabric/releases/download/v1.8.1rc1/libfabric-1.8.1rc1.tar.gz" 
+        wgetdeps "libfabric" "https://github.com/ofiwg/libfabric/releases/download/v1.9.0/libfabric-1.9.0.tar.bz2" 
     fi
 fi
 
