725a53b66972cc32c7de98984c6182215578efee
Jean Luca Bez <jeanlucabez@gmail.com>
2020-02-19 17:15:34 +0100

    update AGIOS installation path to original repository

--- scripts/dl_dep.sh
+++ scripts/dl_dep.sh
@@ -341,7 +341,7 @@ fi
 
 # get AGIOS
 if check_dependency "agios" "${DEP_CONFIG[@]}"; then
-clonedeps "agios" "https://github.com/jeanbez/agios.git" "" "-b gekkofs" &
+clonedeps "agios" "https://github.com/francielizanon/agios.git" "" "-b development" &
 fi
 
 # get date
